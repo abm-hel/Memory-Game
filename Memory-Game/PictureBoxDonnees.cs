38195c5ef077a197bde955d5232678740e0144e8
Adil BEN MOUSSA <adil-benmoussa@hotmail.fr>
2021-06-07 17:16:40 +0200

    Création de la classe qui va contenir les informations des pictures box

---
+++ Memory-Game/PictureBoxDonnees.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory_Game
+{
+
+    [SerializableAttribute]
+    public class PictureBoxDonnees
+    {
+        string Nom;
+        Image Image;
+
+        public string nom
+        {
+            get { return Nom; }
+            set { Nom = value; }
+        }
+
+        public Image image
+        {
+            get { return Image; }
+            set { Image = value; }
+        }
+    }
+}
